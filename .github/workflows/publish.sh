c0cfa494df95f60c5e607791359a7407d25fede0
Eric Bottard <ebottard@pivotal.io>
2020-01-31 16:51:20 +0100

    The error was elsewhere (see previous commit)...

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -8,6 +8,7 @@ gcloud config set disable_prompts True
 gcloud auth activate-service-account --key-file <(echo ${GCLOUD_CLIENT_SECRET} | base64 --decode)
 gcloud auth configure-docker
 
+readonly root=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)
 readonly version=$(cat ${root}/VERSION)
 readonly git_branch=${GITHUB_REF:11} # drop 'refs/head/' prefix
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
@@ -15,8 +16,8 @@ readonly slug=${version}-${git_timestamp}-${GITHUB_SHA:0:16}
 
 publishImage() {
   local tag=$1
-  local source='ko.local/provisioner-735f690b778af94ebf6fae1b426bbf1d:latest'
-  local destination='gcr.io/projectriff/nop-provisioner/provisioner:'${tag}
+  local source=ko.local/provisioner-735f690b778af94ebf6fae1b426bbf1d:latest
+  local destination=gcr.io/projectriff/nop-provisioner/provisioner:${tag}
 
   docker tag ${source} ${destination}
   docker push ${destination}
